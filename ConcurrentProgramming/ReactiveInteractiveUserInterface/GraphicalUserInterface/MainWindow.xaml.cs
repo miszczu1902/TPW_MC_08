b3794052177c0ab2b416208d723ac3dad5abc555
236710 Zuchora Bartosz <236710@edu.p.lodz.pl>
2022-04-14 18:55:39 +0200

    view

--- ConcurrentProgramming/ReactiveInteractiveUserInterface/GraphicalUserInterface/MainWindow.xaml.cs
+++ ConcurrentProgramming/ReactiveInteractiveUserInterface/GraphicalUserInterface/MainWindow.xaml.cs
@@ -1,25 +1,40 @@
-﻿//__________________________________________________________________________________________
-//
-//  Copyright 2022 Mariusz Postol LODZ POLAND.
-//
-//  To be in touch join the community by pressing the `Watch` button and to get started
-//  comment using the discussion panel at
-//  https://github.com/mpostol/TP/discussions/182
-//  with an introduction of yourself and tell us about what you do with this community.
-//__________________________________________________________________________________________
-
+﻿using System;
 using System.Windows;
+using WpfApp1;
 
 namespace TP.ConcurrentProgramming.PresentationView
 {
-  /// <summary>
-  /// View implementation
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    public MainWindow()
+    /// <summary>
+    /// View implementation
+    /// </summary>
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Window1 wind = new Window1();
+        wind.Show();
+        this.Close();
     }
-  }
+
+     public void plus_Click(object sender, RoutedEventArgs e)
+    {
+        int i = Convert.ToInt32(Number.Content);
+        Number.Content = i + 1;
+    }
+    
+     public void minus_click(object sender, RoutedEventArgs e)
+    {
+        int i = Convert.ToInt32(Number.Content);
+    
+        if (i > 0)
+        {
+            Number.Content = i - 1;
+        }
+    }
+}
 }
